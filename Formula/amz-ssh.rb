c4bb9c07d05d2f1cb685e2a49383c5d13e76002b
Jose Diaz-Gonzalez <email@josediazgonzalez.com>
2020-12-14 15:45:23 -0500

    Create amz-ssh.rb

---
+++ Formula/amz-ssh.rb
@@ -0,0 +1,15 @@
+class BrewManage < Formula
+  desc "Tool that enables ec2-instance-connect"
+  homepage "https://github.com/nodefortytwo/amz-ssh"
+  url "https://github.com/nodefortytwo/amz-ssh/releases/download/2.0.1/amz-ssh_2.0.1_darwin_amd64.tar.gz"
+  version "2.0.1"
+  sha256 "157fe6db2cba5ea20b07bbd11d3a01d86d480986d53fb6d8da29d3c07398737a"
+
+  def install
+    bin.install "amz-ssh"
+  end
+
+  test do
+    system "#{bin}/amz-ssh", "-h"
+  end
+end
